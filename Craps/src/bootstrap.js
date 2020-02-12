c738a8c39d6fb2737ed72384b6ab1dc20edf2bdd
cmriddle369 <cmriddle369@gmail.com>
2020-02-12 09:42:56 -0500

    Still fiddling

---
+++ Craps/src/bootstrap.js
@@ -0,0 +1,16 @@
+let wallet = '$100'
+let dieOne = 0
+let dieTwo = 0
+let total = 0
+let points = 0
+let winner = 0
+let loser = 0
+//let players = (user, house)
+//let host = 
+
+/*let host = prompt("Welcome to the Casino! This is our Craps table. You have $100. Would you like to play?");
+    if (host == "yes") {
+        return "Great! Let's get rolling!"
+    } else {
+        return "Too bad. See you next time."
+};*/
