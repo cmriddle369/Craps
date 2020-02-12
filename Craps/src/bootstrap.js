6103f10655cfb3c613ce9965297d4440ca913f10
cmriddle369 <cmriddle369@gmail.com>
2020-02-12 10:06:39 -0500

    rollTheDice func works

--- Craps/src/bootstrap.js
+++ Craps/src/bootstrap.js
@@ -1,12 +1,15 @@
 let wallet = '$100'
-let dieOne = 0
-let dieTwo = 0
+//let dieOne = 0
+//let dieTwo = 0
+//let rollTheDice = 0 find sum 
 let total = 0
 let points = 0
 let winner = 0
 let loser = 0
 //let players = (user, house)
 //let host = 
+// winningNumbers = 7, 11
+// crapsNumbers = 2, 3, 12
 
 /*let host = prompt("Welcome to the Casino! This is our Craps table. You have $100. Would you like to play?");
     if (host == "yes") {
@@ -14,3 +17,13 @@ let loser = 0
     } else {
         return "Too bad. See you next time."
 };*/
+
+// use math to get random die roll //write function 
+function rollTheDice(){
+    var dieOne = Math.floor(Math.random()*6)+1;
+    var dieTwo = Math.floor(Math.random()*6)+1; 
+    rollTheDice = dieOne + dieTwo;
+    return rollTheDice;
+}
+// console.log(rollTheDice());
+
